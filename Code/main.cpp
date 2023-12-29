a189436e94951bae24ef098977164f9dcf7cf1df
D-Bone <38508807+V0ID12@users.noreply.github.com>
2023-12-28 19:46:38 -0600

    Revert "Created a "colorful" window"

--- Code/main.cpp
+++ Code/main.cpp
@@ -4,37 +4,5 @@
 
 int main()
 {
-	glfwInit();
-
-	//GLFW window OpenGL setup
-	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
-	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE); // Uses modern functions
-
-	GLFWwindow* window = glfwCreateWindow(800, 800, "OpenGLTutorial", NULL, NULL);
-	if (window == NULL)
-	{
-		std::cout << "Failed to create the GLFW window...Exiting." << std::endl;
-		glfwTerminate();
-		return -1;
-	}
-	glfwMakeContextCurrent(window);
-
-	gladLoadGL();
-
-	glViewport(0, 0, 800, 800);
-
-	glClearColor(0.07f, 0.13f, 0.17f, 1.0f);
-	glClear(GL_COLOR_BUFFER_BIT);
-	glfwSwapBuffers(window);
-
-	while (!glfwWindowShouldClose(window))
-	{
-		glfwPollEvents();
-
-	}
-
-	glfwDestroyWindow(window);
-	glfwTerminate();
 	return 0;
 }
